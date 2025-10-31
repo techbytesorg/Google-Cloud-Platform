f18ff8d1f04f504465cc5caa6f1742c05fcfe87d
kedar-k <kedarkodgire.kk@gmail.com>
2025-10-31 18:55:35 -0300

    restructure

---
+++ Module5/Demo2/api_call.py
@@ -0,0 +1,21 @@
+
+# run in cloud shell
+from google import genai
+from google.genai import types
+client = genai.Client(
+  vertexai=True, project="YOUR_PROJECT_ID", location="global",
+)
+# If your image is stored in Google Cloud Storage, you can use the from_uri class method to create a Part object.
+IMAGE_URI = "gs://generativeai-downloads/images/scones.jpg"
+model = "gemini-2.5-pro"
+response = client.models.generate_content(
+  model=model,
+  contents=[
+    "What is shown in this image?",
+    types.Part.from_uri(
+      file_uri=IMAGE_URI,
+      mime_type="image/png",
+    ),
+  ],
+)
+print(response.text, end="")
